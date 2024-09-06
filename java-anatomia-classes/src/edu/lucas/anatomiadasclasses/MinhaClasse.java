828743ba0a6cdda7fb710e449b8ac27a7b8df2b1
lucashoriisant <luck201211@hotmail.com>
2024-09-06 05:49:40 -0300

    Inclusão do diretório java-anatomia-classes

---
+++ java-anatomia-classes/src/edu/lucas/anatomiadasclasses/MinhaClasse.java
@@ -0,0 +1,18 @@
+package edu.lucas.anatomiadasclasses;
+public class MinhaClasse{
+
+
+    public static void main (String [] args) {
+
+        String primeiroNome = "Lucas";
+        String segundoNome = "Horii";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
