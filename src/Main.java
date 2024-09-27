2f0f4d162e41596deb3c5a3a5f6f481bdc9d956c
lucashoriisant <luck201211@hotmail.com>
2024-09-26 23:57:00 -0300

    Inclusão do diretório debugging-java

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Iniciou o programa no método main.");
+        a();
+        System.out.println("Finalizou o programa no método main.");
+    }
+
+    public static void a(){
+        System.out.println("Entrou no método a.");
+        b();
+        System.out.println("Finalizou o método a.");
+    }
+
+    public static void b(){
+        System.out.println("Entrou no método b.");
+        for(int i = 0; i<=4; i++) System.out.println(i);
+        c();
+        System.out.println("Finalizou o método b.");
+    }
+
+    public static void c(){
+        System.out.println("Entrou no método c.");
+        Thread.dumpStack();
+        System.out.println("Finalizou o método c.");
+    }
+}
