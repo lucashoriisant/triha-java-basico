3c3e2096817813945d408b5ec2fbac1b30cebe75
lucashoriisant <luck201211@hotmail.com>
2024-11-04 17:49:45 -0300

    Inclusao da classe Atendente.java

---
+++ poo/exemplo-modificadores-de-acesso.java/lanchonete.java/src/lanchonete/Atendente.java
@@ -0,0 +1,19 @@
+package lanchonete;
+
+public class Atendente {
+    public void servindoMesa(){
+        System.out.println("Servindo a mesa");
+   }
+    public void pegarLancheCozinha(){
+        System.out.println("Pegando o lanche na cozinha"); 
+   }
+    public void receberPagamento(){
+        System.out.println("Recebendo o pagamento");
+   }
+   public void trocarGas(){
+        System.out.println("Atendente trocando o gas");
+   }
+   public void pegarPedidoBalcao(){
+        System.out.println("Pegando o pedido no balcao");
+   }
+}
