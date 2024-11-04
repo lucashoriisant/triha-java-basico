c963543da2bde2cd577e8fa127d064c1c1c7b81d
lucashoriisant <luck201211@hotmail.com>
2024-11-04 17:57:08 -0300

    Inclusao da classe Cliente.java

---
+++ poo/exemplo-modificadores-de-acesso.java/lanchonete.java/src/lanchonete/Cliente.java
@@ -0,0 +1,19 @@
+package lanchonete;
+
+public class Cliente {
+    public void escolherLanche(){
+        System.out.println("Escolhendo o lanche");
+    }
+    public void fazerPedido(){
+        System.out.println("Fazendo o pedido");
+    }
+    public void pagarConta(){
+        System.out.println("Pagando a conta");
+    }
+    public void consultarSaldoAplicativo(){
+        System.out.println("Consultando saldo no aplicativo");
+    }
+    public void pegarPedidoBalcao(){
+        System.out.println("Pegando o pedido no balcao");
+    }
+}
