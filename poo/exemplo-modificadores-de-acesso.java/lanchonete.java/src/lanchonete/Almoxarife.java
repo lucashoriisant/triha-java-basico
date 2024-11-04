084dae9a978f8db0d7c9e2708d5668b4ac15c1b6
lucashoriisant <luck201211@hotmail.com>
2024-11-04 17:44:22 -0300

    Inclusao da classe Almoxarife.java

---
+++ poo/exemplo-modificadores-de-acesso.java/lanchonete.java/src/lanchonete/Almoxarife.java
@@ -0,0 +1,16 @@
+package lanchonete;
+
+public class Almoxarife {
+    public void controlarEntrada(){
+        System.out.println("Controlando a entrada dos itens");
+    }
+    public void controlarSaida(){
+        System.out.println("Controlando a saida dos itens");
+    }
+    public void entregarIngredientes(){
+        System.out.println("Entregando ingredientes");
+    }
+    public void trocarGas(){
+        System.out.println("Almoxarife trocando o gás");
+    }
+}
