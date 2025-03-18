23a1c48d2f23aeb2b88243c00bea8419615eefca
Lucas Horii dos Santos <67847769+lucashoriisant@users.noreply.github.com>
2025-03-18 18:10:54 -0300

    Inclusão do exemplo Autodromo.java (Classes: Veiculo.java, Carro.java e Moto.java

---
+++ poo/pilares-poo/Carro.java
@@ -0,0 +1,17 @@
+public class Carro extends Veiculo {
+
+    public void ligar() {
+        confereCambio();
+        confereCombustivel();
+        System.out.println("CARRO LIGADO");
+    }
+
+    private void confereCombustivel() {
+        System.out.println("Conferindo combustível");
+    }
+
+    private void confereCambio() {
+        System.out.println("Conferindo câmbio em P");
+    }
+
+}
