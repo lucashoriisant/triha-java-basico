df4f916324575b3ee8f09d004be5e383fa9a352b
Lucas Horii dos Santos <67847769+lucashoriisant@users.noreply.github.com>
2025-03-19 11:41:50 -0300

    Inclusão da classe MSNMessenger.java

---
+++ poo/pilares-poo/exemplo-msn/MSNMessenger.java
@@ -0,0 +1,18 @@
+public class MSNMessenger {
+    public void enviarMensagem() {
+        System.out.println("Enviando mensagem");
+    }
+
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem");
+    }
+
+    public void validarConectadoInternet() {
+        System.out.println("Validando se está conectado na internet");
+    }
+
+    public void salvarHistoricoMensagem() {
+        System.out.println("Salvando o histórico de mensagem");
+    }
+
+}
